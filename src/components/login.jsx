5fd6af7e9a2d929739173fd04bd6cc7406e29fc7
Mohammadshuvouup <mohammad.ali.shuvo161@gmail.com>
2024-06-01 13:18:35 +0600

    loging component updated

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,57 +1,73 @@
 import { Field, Input, Label, Button } from "@headlessui/react";
 import clsx from "clsx";
-import { useForm } from "react-hook-form"
-import { useEffect } from 'react';
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
 import axios from "axios";
 
 const Login = () => {
-  const { register, handleSubmit } = useForm({
+  const { register, handleSubmit, watch } = useForm({
     defaultValues: {
-      userName: '',
-      password: ''
-    }
+      userName: "",
+      password: "",
+    },
   });
+  const onSubmit = (data) => {
+    console.log(data);
+    axios
+    .post("https://dummyjson.com/auth/login", { username:data.userName, password: data.password })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error.data);
+    });
+  }
+  
+  
+  
+  
+
+
+
 
   // useEffect(() => {
   //   axios
   //     .post('https://dummyjson.com/auth/login')
-  //     .then((response) => {
+  //     .then((handleSubmit) => {
   //       // setLogin(response.json);
-  //       console.log(response.json);
+  //       console.log(handleSubmit.json);
   //     })
-   
+
   // }, [])
-  
+
   return (
     <>
-    <form onSubmit={handleSubmit(console.log)}>
-
-      <div className="w-[380px] m-auto bg-black p-28 rounded-lg ">
-        <Field>
-          <Label className="text-sm/6 font-medium text-white">
-            Sign in
-          </Label>
-          <Input
-            placeholder="Enter your email"
-            className={clsx(
-              "mt-3 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white",
-              "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
-            )}
-          />
-          <Input
-            placeholder="Password"
-            required=""
-            className={clsx(
-              "mt-3 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white",
-              "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
-            )}
-          />
-          <Button className="mt-20 rounded bg-sky-600 py-2 px-4 text-sm text-white data-[hover]:bg-sky-500 data-[active]:bg-sky-700">
-            Submit
-          </Button>
-        </Field>
-      </div>
-    </form>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="w-[380px] m-auto bg-black p-28 rounded-lg ">
+          <Field>
+            <Label className="text-sm/6 font-medium text-white">Sign in</Label>
+            <Input
+              {...register("trn_no", { required: true })}
+              placeholder="Enter your email"
+              className={clsx(
+                "mt-3 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white",
+                "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
+              )}
+            />
+            <Input
+              placeholder="Password"
+              required=""
+              className={clsx(
+                "mt-3 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white",
+                "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
+              )}
+            />
+            <Button type="submit" className="mt-20 rounded bg-sky-600 py-2 px-4 text-sm text-white data-[hover]:bg-sky-500 data-[active]:bg-sky-700">
+              Submit
+            </Button>
+          </Field>
+        </div>
+      </form>
     </>
   );
 };
